e7851b6da88f725a9781f75bb3c79330e11d08f0
accaicedtea <abdelaziz.1941255@studenti.uniroma1.it>
2023-03-31 21:22:05 +0200

    head php
    head dinamico

--- site/login.php
+++ site/login.php
@@ -1,42 +1,55 @@
-<?php include './head.html';?>
-
 <?php 
-session_start();
-?>
-<body>
-  <div class="row mb-3">
-    <div class="col-lg-3"></div>
-    <div class="col-lg-6">
-      <div class="card shadow mt-5">
-        <div id="wrapper">
-          <div class="d-flex flex-column" id="content-wrapper">
+
+$_SESSION['pagina'] = 'Login';
+include './head.php';?>
+<body id="page-top">
+    <div id="wrapper">
+        <div class="d-flex flex-column" id="content-wrapper">
             <div id="content">
-              <div class="container-fluid">
-                <form class="form-signin">
-                  <div row class="text-center">
-                    <img class="mt-3 text-center" src="./assets/brand/4M-cropped.svg" alt="" width="200" height="200">
-                  </div>
-                  <p class="h3 text-dark mb-4 mt-3 text-center">Accedi</p>
-                  <div class="text-start"><label class="form-label" for="inputEmail"><strong>Indirizzo email</strong></label></div>
-                  <input type="email" id="inputEmail" class="form-control" placeholder="example@gmail.com" required autofocus>
-                  <p></p>
-                  <div class="text-start"><label class="form-label" for="inputPassowrd"><strong>Password</strong></label></div>
-                  <input type="password" id="inputPassword" class="form-control" placeholder="************" required>
-                  <p><br></p>
-                  <div class="text-center"><button class="btn btn-lg btn-primary btn-block" type="submit">Accedi</button></div>
-                  <p></p>
-                  <div class="mb-4">
-                    Non hai un account?
-                    <a href="./register.html">Registrati</a>
-                  </div>
-                </form>
-              </div>
+                <!-- per farlo più piccolino se levi fluid -->
+                <div class="container-fluid">
+                    <div class="row mb-3">
+                        <!-- lg -3 col-->
+                        <div class="col-lg-3"></div>
+                        <div class="col-lg-6">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="card shadow mt-5 mb-3">
+                                      <div row class="text-center">
+                                        <img class="mt-3 text-center" src="./assets/brand/4M-cropped.svg" alt="" width="200" height="200">
+                                      </div>
+                                        <div class="row-md-3">
+                                            <p class="h3 text-center mb-3 mt-3">Accedi</p>
+                                        </div>
+                                        <div class="card-body">
+                                            <form action="./login_user.php" method="post" name="form-signin" class="form-signin">
+                                                <div class="row">
+                                                    <div class="col">
+                                                        <div class="mb-3"><label class="form-label" for="email"><strong >Inidirizzo email</strong></label><input class="form-control" type="email" id="email" placeholder="example@email.com" name="email"></div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col">
+                                                        <div class="mb-5"><label class="form-label" for="password"><strong >Password</strong></label><input class="form-control" type="password" id="passord" placeholder="************" name="password"></div>
+                                                    </div>
+                                                </div>
+                                                    <div class="text-center mb-3"><button class="btn btn-lg btn-primary btn-block" type="submit">Accedi</button></div>
+                                                    </div>
+                                                    <div class="mb-4 text-center">  <span class="h6 text-center">  Non hai un account? </span>
+                                                      <a href="./register.php">Registrati</a>
+                                                    </div>
+                                                </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3"></div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
+        </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
     </div>
-    <div class="col-lg-3"></div>
-  </div>
 </body>
+
 </html>
