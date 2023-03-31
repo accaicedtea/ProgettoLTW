de13c79f30fbf513f77fc4adb41a86150e11de8f
accaicedtea <107927176+accaicedtea@users.noreply.github.com>
2023-03-31 16:56:29 +0200

    f
    gestione errori

--- site/db_conn.php
+++ site/db_conn.php
@@ -1,9 +1,11 @@
 <?php
 
-    $conn = mysqli_connect("localhost", "root", "", "cd");
-    // Check connection
-    if($conn === false){
-        die("ERROR: Could not connect. "
-            . mysqli_connect_error());
+mysqli_report(MYSQLI_REPORT_OFF);
+/* @ is used to suppress warnings */
+$link = @mysqli_connect('localhost', 'root', '', 'cd');
+    if (!$link) {
+    /* Use your preferred error logging method here */
+        header("Location: ./error.php?error=405");
+        exit();   
     }
 ?>
