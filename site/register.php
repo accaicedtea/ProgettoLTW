e923e6bcf974a8d1a38c13e142ea7ed27bea35e9
Cristyl <cristianmm1240@gmail.com>
2023-04-01 17:17:52 +0200

    Finiti i controlli
    Controlli fatti ma tocca rivedere lo stile di segnalazione degli errori

--- site/register.php
+++ site/register.php
@@ -1,7 +1,7 @@
 <?php include './head.html';?>
 
 <script src="./register_controlli.js"></script>
-
+<link rel="stylesheet" href="./register_errors.css">
 <body id="page-top" style="background-color:#e9e9e9;">
     <div id="wrapper">
         <div class="d-flex flex-column" id="content-wrapper">
@@ -23,18 +23,21 @@
                                             <form action="./register_user.php" method="post" name="formreg" onsubmit="return validaForm();">
                                                 <div class="row">
                                                     <div class="col">
-                                                        <div class="mb-3"><label class="form-label" for="username"><strong >Nome utente</strong></label><input class="form-control" type="text" id="username" placeholder="nome utente" name="username" onfocus="show_requirements_username()" onblur="remove_requirements_username()" onchange="return error_requirements_username()"></div>
-                                                        <div id=requirements_username></div>
+                                                        <div class="mb-3"><label class="form-label" for="username"><strong >Nome utente</strong></label><input class="form-control" type="text" id="username" placeholder="nome utente" name="username" onfocus="show_requirements_username()" onblur="remove_requirements_username()" onchange="remove_error_username()"></div>
+                                                        <div class="mb-3" id=requirements_username></div>
+                                                        <div class="errors mb-3" id="errors_username"></div>
                                                     </div>
                                                 </div>
                                                 <div class="row">
                                                     <div class="col">
-                                                        <div class="mb-3"><label class="form-label" for="nome"><strong >Nome</strong></label><input class="form-control" type="text" id="nome" placeholder="solo nome" name="nome"></div>
+                                                        <div class="mb-3"><label class="form-label" for="nome"><strong >Nome</strong></label><input class="form-control" type="text" id="nome" placeholder="solo nome" name="nome" onchange="remove_error_name()"></div>
+                                                        <div class="errors mb-3" id="errors_name"></div>
                                                     </div>
                                                 </div>
                                                 <div class="row">
                                                     <div class="col">
-                                                        <div class="mb-3"><label class="form-label" for="cognome"><strong >Cognome</strong></label><input class="form-control" type="text" id="cognome" placeholder="cognome" name="cognome"></div>
+                                                        <div class="mb-3"><label class="form-label" for="cognome"><strong >Cognome</strong></label><input class="form-control" type="text" id="cognome" placeholder="cognome" name="cognome" onchange="remove_error_cognome()"></div>
+                                                        <div class="errors mb-3" id="errors_cognome"></div>
                                                     </div>
                                                 </div>
                                                 <div class="row">
@@ -50,26 +53,28 @@
                                                 
                                                 <div class="row">
                                                     <div class="col">
-                                                        <div id="requirements_dataN"></div>
-                                                        <div class="mb-3"><label class="form-label" for="dataN"><strong >Data di nascita</strong></label><input class="form-control" type="date" id="dataN" name="dataN" onfocus="show_requirements_dataN()" onblur="remove_requirements_dataN()"></div>
+                                                        <div class="requirements mb-3" id="requirements_dataN"></div>
+                                                        <div class="errors mb-3" id="errors_dataN"></div>
+                                                        <div class="mb-3"><label class="form-label" for="dataN"><strong >Data di nascita</strong></label><input class="form-control" type="date" id="dataN" name="dataN" onfocus="show_requirements_dataN()" onblur="remove_requirements_dataN()" onchange="remove_error_dataN()"></div>
                                                     </div>
                                                 </div>
                                                 <div class="row">
                                                     <div class="col">
-                                                        <div class="mb-3"><label class="form-label" for="email"><strong >Inidirizzo email</strong></label><input class="form-control" type="email" id="email" placeholder="example@email.com" name="email"></div>
+                                                        <div class="mb-3"><label class="form-label" for="email"><strong >Inidirizzo email</strong></label><input class="form-control" type="email" id="email" placeholder="example@email.com" name="email" onchange="remove_error_email()"></div>
+                                                        <div class="errors mb-3" id="errors_email"></div>
                                                     </div>
                                                 </div>
                                                 <div class="row">
                                                     <div class="col">
-                                                        <div class="mb-3"><label class="form-label" for="password"><strong >Password</strong></label><input class="form-control" type="password" id="password" placeholder="*****" name="password" onfocus="show_requirements_password()" onblur="remove_requirements_password()" onchange="return error_requirements_password()"></div>
-                                                        <div id="requirements_password"></div>
+                                                        <div class="mb-3"><label class="form-label" for="password"><strong >Password</strong></label><input class="form-control" type="password" id="password" placeholder="*****" name="password" onfocus="show_requirements_password()" onblur="remove_requirements_password()" onchange="remove_error_password()"></div>
+                                                        <div class="mb-3" id=requirements_password></div>
+                                                        <div class="errors mb-3" id="errors_password"></div>
                                                     </div>
                                                 </div>
                                                 <div class="col">
-                                                        <div class="mb-5"><label class="form-label" for="passwordC"><strong >Conferma password</strong></label><input class="form-control" type="password" id="password" placeholder="*****" name="passwordC" onchange="return error_confirm_password()"></div>
-                                                        <div id="confirm_password"></div>
+                                                        <div class="mb-3"><label class="form-label" for="passwordC"><strong >Conferma password</strong></label><input class="form-control" type="password" id="passwordC" placeholder="*****" name="passwordC" onchange="remove_error_passwordC()"></div>
+                                                        <div class="errors mb-3" id="errors_confirm_password"></div>
                                                     </div>
-                                                
                                                     <div class="mb-3"><button class="btn btn-primary btn-sm" type="submit">Registrati</button></div>
                                                     </div>
                                                 </form>
--- site/register_controlli.js
+++ site/register_controlli.js
@@ -7,7 +7,7 @@ function remove_requirements_username() {
 }
 
 function show_requirements_password() {
-    document.getElementById("requirements_password").innerHTML = "La password deve contenere:<br>1 carattere maiscolo;<br>1 carattere minuscolo;<br>Almeno 12 caratteri;<br>1 numero;<br>1 caratterespeciale (!@#$%^&*)"
+    document.getElementById("requirements_password").innerHTML = "Requisiti password:<ul><li>Lunghezza compresa tra 6 e 20 caratteri</li><li>1 carattere maiuscolo</li><li>1 carattere minuscolo</li><li>1 numero</li><li>1 carattere speciale (!@#$%^&*)</li>"
 }
 
 function remove_requirements_password() {
@@ -23,23 +23,78 @@ function remove_requirements_dataN() {
 }
 
 function validaForm() {
-    var password_regex = RegExp("(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,20}")
+    var password_regex = RegExp("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&])(?=.*[a-zA-Z0-9]).{6,20}")
     var password = document.formreg.password.value;
+    var today = new Date();
+    var dataN = new Date(document.formreg.dataN.value);
     if(document.formreg.username.value=="") {
-        document.getElementById("requirements_username").innerHTML = "Errore! Inserisci username";
+        document.getElementById("errors_username").innerHTML = "Errore! Inserisci username";
+        return false;
+    }
+    if(document.formreg.nome.value=="") {
+        document.getElementById("errors_name").innerHTML = "Errore! Inserisci un nome";
+        return false;
+    }
+    if(document.formreg.cognome.value=="") {
+        document.getElementById("errors_cognome").innerHTML = "Errore! Inserisci un cognome";
+        return false;
+    }
+    if (document.formreg.dataN.value== "") {
+        document.getElementById("errors_dataN").innerHTML = "Errore! Inserisci una data di nascita";
+        return false;
+    }
+    if (today.getFullYear() - dataN.getFullYear() < 14) {
+        document.getElementById("errors_dataN").innerHTML = "Errore! Data non valida";
+        return false;
+    }
+    if (today.getFullYear() - dataN.getFullYear() == 14) {
+        if(today.getMonth() < dataN.getMonth()) {
+            document.getElementById("errors_dataN").innerHTML = "Errore! Data non valida";
+            return false;
+        }
+        if(today.getMonth() == dataN.getMonth()) {
+            if (today.getDate() < dataN.getDate()) {
+                document.getElementById("errors_dataN").innerHTML = "Errore! Data non valida";
+                return false;
+            }
+        }
+    }
+    if (document.formreg.email.value == "") {
+        document.getElementById("errors_email").innerHTML = "Errore! Inserisci una email";
         return false;
     }
     if (password==""){
-        document.getElementById("requirements_password").innerHTML = "Errore! Inserisci una password";
+        document.getElementById("errors_password").innerHTML = "Errore! Inserisci una password";
         return false;
     }
     if (!password.match(password_regex)) {
-        document.getElementById("requirements_password").innerHTML = "Formato password non valido";
+        document.getElementById("errors_password").innerHTML = "Errore! Formato password non valido";
         return false;
     }
     if (document.formreg.password.value != document.formreg.passwordC.value) {
-        document.getElementById("confirm_password").innerHTML = "Errore! Le password non coincidono";
+        document.getElementById("errors_confirm_password").innerHTML = "Errore! Le password non coincidono";
         return false;
     }
     return true;
+}
+function remove_error_username() {
+    document.getElementById("errors_username").innerHTML = "";
+}
+function remove_error_name() {
+    document.getElementById("errors_name").innerHTML = "";
+}
+function remove_error_cognome() {
+    document.getElementById("errors_cognome").innerHTML = "";
+}
+function remove_error_dataN() {
+    document.getElementById("errors_dataN").innerHTML = "";
+}
+function remove_error_email() {
+    document.getElementById("errors_email").innerHTML = "";
+}
+function remove_error_password() {
+    document.getElementById("errors_password").innerHTML = "";
+}
+function remove_error_passwordC() {
+    document.getElementById("errors_confirm_password").innerHTML = "";
 }
