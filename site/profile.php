e000f176c55e4c1c9c1511f27a6ec2b9bd47b9cd
Cristyl <cristianmm1240@gmail.com>
2023-04-05 14:35:14 +0200

    sus

--- site/profile.php
+++ site/profile.php
@@ -91,7 +91,7 @@
                     </div>
                 </div>
             </div>
-        </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
+        </div><a class="border rounded d-inline scroll-to-top" href="#top"><i class="fas fa-angle-up"></i></a>
     </div>
 </body>
 
