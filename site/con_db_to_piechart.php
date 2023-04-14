0f24cbfb84aa7985b3dd0803974ba418a70f5670
accaicedtea <107927176+accaicedtea@users.noreply.github.com>
2023-04-14 23:09:24 +0200

    Update con_db_to_piechart.php

--- site/con_db_to_piechart.php
+++ site/con_db_to_piechart.php
@@ -3,7 +3,7 @@ $pagina = "con_db_to_dashboard";
 $username = $_SESSION['username'];
 $_SESSION['data_oggi'] = date("Y:m:d");
 $data_oggi = $_SESSION['data_oggi'];
-$sql = "SELECT categoria as categoria, sum(importo) / (SELECT sum(importo) from spesa where importo < 0 AND MONTH(spesa.data) = MONTH('$data_oggi') AND spesa.utente = '$username') as somma, categoria.colore as colore from spesa join categoria on categoria.id = spesa.categoria where spesa.importo < 0 AND spesa.utente = '$username' AND MONTH(spesa.data) = MONTH('$data_oggi') group by categoria.nome";
+$sql = "SELECT categoria.nome as categoria, sum(importo) / (SELECT sum(importo) from spesa where importo < 0 AND MONTH(spesa.data) = MONTH('$data_oggi') AND spesa.utente = '$username') as somma, categoria.colore as colore from spesa join categoria on categoria.id = spesa.categoria where spesa.importo < 0 AND spesa.utente = '$username' AND MONTH(spesa.data) = MONTH('$data_oggi') group by categoria.nome";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
