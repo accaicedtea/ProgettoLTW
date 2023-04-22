35e5af28fc20097dd0ccf1ee48ef492a7c517e05
accaicedtea <abdelaziz.1941255@studenti.uniroma1.it>
2023-04-22 11:36:52 +0200

    ll

--- site/head.php
+++ site/head.php
@@ -15,6 +15,7 @@
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script><!-- jquery-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide|Sofia|Trirong"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!--<link rel="stylesheet" href="./assets/css/style.css">-->
     <script src="./assets/js/register_controlli.js"></script>
 </head>
--- site/informazioni.php
+++ site/informazioni.php
@@ -3,18 +3,18 @@
   include './head.php';
 ?>
 
-<body class="page-top">
+<body class="page-top" id="page-top">
 <?php 
   include './db_conn.php';
   include './navBar.php';
   if(isset($_SESSION['log']) && $_SESSION['log']== 'on'){
 ?>
-<div id="#page-top"></div>
+<div "></div>
   <hr class="featurette-divider">
     <div class="row featurette">
       <div class="col-md-7">
         <h2 class="featurette-heading">Hasan <span class="text-muted">Abdel Aziz</span></h2>
-        <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+        <p class="lead">My name is Yoshikage Kira. I'm 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married. I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest. I don't smoke, but I occasionally drink. I'm in bed by 11 PM, and make sure I get eight hours of sleep, no matter what. After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, I usually have no problems sleeping until morning. Just like a baby, I wake up without any fatigue or stress in the morning. I was told there were no issues at my last check-up. I'm trying to explain that I'm a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night. That is how I deal with society, and I know that is what brings me happiness. Although, if I were to fight I wouldn't lose to anyone.</p>
       </div>
       <div class="col-md-5">
         <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
@@ -48,8 +48,9 @@
     <hr class="featurette-divider">
 <footer class="container">
 <p>&copy; 2023–2023 4Money, Inc. &middot; <a href="./assets/vid/get rickrolled lol.mp4">Privacy</a> &middot; <a href="./assets/vid/1 Hour Of Silence occasionally broken up by Lego Yoda death sound..mp4">Terms</a></p>
-<a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
+<a id="back-to-top" href="#" class="btn btn-light btn-lg back-to-top " role="button"><i class="fas fa-chevron-up"></i></a>
 </footer>
+  
 </body>
 <?php } ?>
-  
+    
