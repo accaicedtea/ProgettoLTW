f225b9b865663af5341243c4438853e240ca1cba
accaicedtea <107927176+accaicedtea@users.noreply.github.com>
2023-05-10 17:20:59 +0200

    vfine

---
+++ site/sblocca_utente.php
@@ -0,0 +1,19 @@
+<?php
+//TODO: Gestione errori
+require "./funzioni.php";
+ $conn = db_conn();
+$sblockThis = $_POST["sblockthis"];
+$sql = "SELECT password from utente WHERE username='$sblockThis'";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) === 1) {
+    $row = mysqli_fetch_assoc($result);
+    $passw = $row["password"];
+    $passw = rtrim($passw, "adminBlock");
+    $sql = "UPDATE utente SET password = '$passw' WHERE username = '$sblockThis'";
+    if (mysqli_query($conn, $sql)) {
+        header("Location: view.php?msg= sbloccato l'utente ");
+    }else{
+        header("Location: view.php?error=qualcosa è andato storto");
+    }
+}
+?>
