efdf4aa8f6f182196f1292c215d51beaf47f8f69
accaicedtea <abdelaziz.1941255@studenti.uniroma1.it>
2023-05-14 20:56:09 +0200

    vfine

--- site/index.php
+++ site/index.php
@@ -4,31 +4,85 @@ $pagina = "Home";
 require './funzioni.php';
 $conn = db_conn();
 head($pagina);
-navBar($pagina, "PER FAR CAPIRE IN CHE PAGINA SIAMO");
+navBar($pagina, "");
 
 
 
 ?>
+<style>
+#sipario{
+  display: none;
+}
+</style>
+<body>
+<div id="conenuto" class="text-center mt-5">
+  <div class="container">
+    <div class="card shadow">
+      <div class="card-body">
+        <h1 class="mt-3"><strong>HOME PAGE</strong></h1>
+        <img class="mt-3 mb-3 text-center" src="./assets/brand/4M-cropped.svg" alt="" width="200" height="200">
+        <p class="lead">Ciao se hai un account puoi <a class=" zoom" href="./login.php" role="button">accedere</a> 
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <a class="navbar-brand logo-nav" href="./home.php">4Money</a>
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item mt-2">
-                    <a class="link-navbar  aria-current="page" href="./home.php">HOME
-                    </a>
-                </li>
-                <li class="nav-item mt-2 ms-3">
-                    <a class=" link-active" href="./informazioni.php">INFORMAZIONI
-                    </a>
-                </li>
-                </ul>
-                <a class="btn me-3 btn-success zoom" href="./login.php" role="button">Accedi</a>
-                <a class="btn btn-danger zoom" href="./register.php  " role="button">Registrati</a>
-            </div>
-            </div>
-        </nav>
+        altrimenti puoi crare un account <a class="zoom" href="./register.php  " role="button"> clicca qui per registrati</a></p>
+        
+        <button class="btn1 btn btn-lg text-white btn-secondary fw-bold border-white bg-dark zoom">SCOPRI DI PIU'</button>
+      </div>
+    </div> 
+  </div>
+  </p> 
+</div>
+
+
+<div id="sipario" class="text-center mt-5">
+
+  <button class="btn2 btn btn-lg text-dark btn-secondary fw-bold border-white bg-white">HOME</button>
+
+  <!-- Marketing messaging and featurettes
+  ================================================== -->
+  <!-- Wrap the rest of the page in another container to center all the content. -->
+
+  <div class="container marketing">
+
+    
+    <!-- START THE FEATURETTES -->
+
+    <hr class="featurette-divider">
+
+    
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
+        <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <hr class="featurette-divider">
+
+    <!-- /END THE FEATURETTES -->
+
+  </div><!-- /.container -->
+
+  
+</div>
+</body>
+
+<script>
+$(document).ready(function(){
+  $(".btn1").click(function(){
+    $("#conenuto").slideToggle();
+    $("#sipario").slideToggle();
+  });
+  $(".btn2").click(function(){
+    $("#conenuto").slideToggle();
+    $("#sipario").slideToggle();
+  });
+});
+</script>
