6424dedd44f0c59a7e4adfdaf6f220c67fe0ab24
accaicedtea <107927176+accaicedtea@users.noreply.github.com>
2023-05-16 16:59:13 +0200

    vifne

---
+++ site/modifica_categoria.php
@@ -0,0 +1,20 @@
+<?php
+require './funzioni.php';
+$conn= db_conn();
+$id = $_POST['id_edit'];
+        $nome =  $_POST['nome_edit'];
+        $img =  $_POST['img_edit'];
+        $colore =  $_POST['colore_edit'];
+        // Performing insert query execution
+        $sql = "UPDATE categoria SET nome = '$nome', colore = '$colore', img = '$img' WHERE id = '$id'";
+
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+        $query = mysqli_query($conn, $sql);
+
+        if($query){
+            
+            header("Location: categorie.php?msg=modificato la categoria");
+        } else{
+            header("Location: categorie.php?error=Qualcosa è andato storto :(");
+        }
+?>
