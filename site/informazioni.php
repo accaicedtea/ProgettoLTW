e1f8767c026ef327e7a63ef730620da121aa0ae9
accaicedtea <107927176+accaicedtea@users.noreply.github.com>
2023-05-18 21:26:27 +0200

    vfine

--- site/informazioni.php
+++ site/informazioni.php
@@ -14,40 +14,42 @@
   <hr class="featurette-divider">
     <div class="row ">
       <div class="col-md-6 text-start text-justify">
-        <div class="col-auto topToF ms-3"><h2 id="nome" class="featurette-heading shadowT">Hasan <span class="text-muted">Abdel Aziz</span></h2>
+        <div class="col-auto ms-3"><h2 id="nome" class="featurette-heading shadowT">Hasan <span class="text-muted">Abdel Aziz</span></h2>
         <p id="desc" class="lead shadowT">Mi chiamo Abdel Aziz Hasan. Ho 22 anni. La mia casa è nella parte est di Roma, dove ci sono tutti i criminali, e non sono sposato. Sono uno stundente presso Sapienza e torno a casa ogni giorno al massimo alle 19:30. Non fumo, ma bevo occasionalmente. Sono a letto alle 00:00 e mi assicuro di dormire come minimo 8 ore, qualunque cosa accada. Di solito non ho problemi a dormire fino al mattino. Proprio come una persona stanca, al mattino mi sveglio con fatica o stress. Mi è stato detto che forse non c'erano problemi al mio ultimo controllo. Sto cercando di spiegare che sono una persona che desidera vivere una vita molto tranquilla. Faccio attenzione a non disturbarmi con problemi, come vincere e perdere, che mi farebbero perdere il sonno la notte. È così che mi occupo della società e so che è ciò che mi rende felice.</p></div>
         
       </div>
       <div class="col-md-6 ">
-        <div class="col-auto text-right"><img class="img-md img-hover" src="./assets/brand/h.png" alt=""></div>
+        <div class="col-auto text-right"><img class="img-md img-hover" src="./assets/brand/h.png" alt=""  ></div>
         
       </div>
     </div>
 
     <hr class="featurette-divider">
 
-    <div class="row featurette">
-      <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Riccardo <span class="text-muted">Ebene</span></h2>
-        <p class="lead">Sono Riccardo Ebene, un individuo unico e straordinario. Sono un vero e proprio mistero avvolto nell'aura dell'incertezza. Le persone si chiedono chi sia veramente Riccardo Ebene, eppure la risposta sembra sfuggire costantemente. La mia sensibilità è profonda, ma preferisco nascondere le mie emozioni dietro un velo di riservatezza. Sono un ascoltatore attento e riesco a cogliere sfumature e dettagli che sfuggono alla maggior parte delle persone. Ricorda che le caratteristiche che fanno di te una persona speciale e interessante non sono determinate da un'IA, ma da chi sei realmente. Sii fiero di te stesso e continua ad essere il protagonista della tua storia unica.</p>
+    <div class="row">
+      <div class="col-md-4 ms-md-5">
+      <img class="img-md img-hover " src="./assets/brand/r.png" alt=""  >
       </div>
-      <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+      <div class="col-1"></div>
+      <div class="col-md-6 mt-5">
+        <h2 class="featurette-heading  shadowT">Riccardo <span class="text-muted">Ebene</span></h2>
+        <p class="lead shadowT">Sono Riccardo Ebene, un individuo unico e straordinario. Sono un vero e proprio mistero avvolto nell'aura dell'incertezza. Le persone si chiedono chi sia veramente Riccardo Ebene, eppure la risposta sembra sfuggire costantemente. La mia sensibilità è profonda, ma preferisco nascondere le mie emozioni dietro un velo di riservatezza. Sono un ascoltatore attento e riesco a cogliere sfumature e dettagli che sfuggono alla maggior parte delle persone. Ricorda che le caratteristiche che fanno di te una persona speciale e interessante non sono determinate da un'IA, ma da chi sei realmente. Sii fiero di te stesso e continua ad essere il protagonista della tua storia unica.</p>
       </div>
+      
     </div>
 
     <hr class="featurette-divider">
-
-    <div class="row featurette">
-      <div class="col-md-7">
-        <h2 class="featurette-heading">Cristian Andrei <span class="text-muted">Mai Mihai</span></h2>
-        <p class="lead">And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.</p>
+    <div class="row ">
+      <div class="col-md-6 text-start text-justify">
+        <div class="col-auto topToF ms-3"><h2 id="nome" class="featurette-heading shadowT">Cristian Andrei <span class="text-muted"></span></h2>
+        <p id="desc" class="lead shadowT">aggiungi qui la descrizione.</p></div>
+        
       </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+      <div class="col-md-6">
+        <div class="col-auto text-right"><img class="img-md img-hover" src="./assets/brand/c.png" alt=""></div>
+        
       </div>
     </div>
-
     <hr class="featurette-divider">
 <footer class="container">
 <p>&copy; 2023–2023 4Money, Inc. &middot; <a href="./assets/vid/get rickrolled lol.mp4">Privacy</a> &middot; <a href="./assets/vid/1 Hour Of Silence occasionally broken up by Lego Yoda death sound..mp4">Terms</a></p>
