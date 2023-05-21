29bb6c8cefe0b7d3bf56c64c9e571bf2b90460fc
accaicedtea <107927176+accaicedtea@users.noreply.github.com>
2023-05-21 14:07:09 +0200

    fine

--- fine/categorie.php
+++ fine/categorie.php
@@ -22,7 +22,7 @@
                         </div>
                         <?php }else if(isset($_GET['msg'])){?>
                         <div class="alert alert-info alert-dismissible fade show" role="alert">
-                            <strong>Operazione eseguita! hai correttamente</strong><?php echo $_GET['msg']; ?>
+                            <strong>Operazione eseguita! hai correttamente </strong><?php echo $_GET['msg']; ?>
                             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                         </div>
                         <?php }?>
